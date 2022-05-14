f9809390df232eec15272df9c7fc7d8006aad67c
kraskden <Fizzika.NET@yandex.ru>
2022-05-14 16:04:57 +0300

    Remove tutorial code && typescript

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { NavigationBar } from "./components/navbar/NavBar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,44 @@
+import { Button, Container, Form, FormControl, Nav, Navbar } from "react-bootstrap"
+
+const LINKS =  [
+  {
+    link: '/',
+    name: 'User Rating'
+  },
+  {
+    link: '/compare',
+    name: 'Compare Users'
+  },
+  {
+    link: '/online',
+    name: 'OnlineHub'
+  },
+  {
+    link: '/about',
+    name: 'About'
+  }
+]
+
+export function NavigationBar() {
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand className="fs-4">TankiRating</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="main-navbar-nav" />
+        <Navbar.Collapse id="main-navbar-nav">
+          <Nav className="me-auto">
+            {LINKS.map((l, idx) => (
+              
+              <Nav.Link key={idx} className="fw-semibold mx-lg-3 fs-5">{l.name}</Nav.Link>
+            ))}
+          </Nav>
+          <Nav>
+            <Button variant="success" className="fw-semibold fs-5">Add user</Button>
+          </Nav>
+        </Navbar.Collapse>
+
+      </Container>
+    </Navbar>
+  )
+}
