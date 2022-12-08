e7c2b78eac9d305f5329499d95b2a321ab90f8a5
Fizzika <Fizzika.NET@yandex.ru>
2022-12-08 17:39:19 +0300

    Update about

--- src/components/about/EnAbout.jsx
+++ src/components/about/EnAbout.jsx
@@ -18,7 +18,7 @@ export function EnAbout() {
        <br/>
      </p>
      <p className="fs-5">
-2      Just open <Link to="/">the main page</Link>, and enjoy!
+      Just open <Link to="/">the main page</Link>, and enjoy!
      </p>
  
      <p className="h3 mt-4">
@@ -44,10 +44,22 @@ export function EnAbout() {
      <p className="fs-5">
       If you changed your nickname, want to delete/freeze your account on the portal, or just have something to say:     </p>
      <ul className="ms-4">
-       <li><span className="fs-5"> <a href="https://ru.tankiforum.com/messenger/compose/?to=808" className="href">Write PM</a> on russian forum to <b>Fizzika</b> (recommends)</span></li>
+       <li><span className="fs-5"> <a target="_blank" rel="noopener noreferrer" href="https://ru.tankiforum.com/messenger/compose/?to=808" className="href">Write PM</a> on russian forum to <b>Fizzika</b> (recommends)</span></li>
        <li><span className="fs-5">Email me <a href="mailto:kdby@riseup.net" className="href">kdby@riseup.net</a></span></li>
      </ul>
  
+     <p className="h3 mt-4">
+       <a name="source">Source code, API docs</a>
+     </p>
+
+     <p className="fs-5">TankiRating is the free and open-source project</p>
+
+     <ul className="ms-4">
+       <li><span className="fs-5"> <a target="_blank" rel="noopener noreferrer" href="https://github.com/kraskden/tankirating-app" className="href">Client source</a></span></li>
+       <li><span className="fs-5"> <a target="_blank" rel="noopener noreferrer" href="https://github.com/kraskden/tankirating-api" className="href">API source</a></span></li>
+       <li><span className="fs-5"> <a target="_blank" rel="noopener noreferrer" href="https://tankirating.org/api/swagger-ui/index.html" className="href">API documentation</a></span></li>
+     </ul>
+
     </>
    )
 }
--- src/components/about/RuAbout.jsx
+++ src/components/about/RuAbout.jsx
@@ -3,52 +3,64 @@ import { AboutPictures } from "./AboutPictures";
 
 export function RuAbout() {
   return (
-   <>
-    <p className="fs-5">
-      TankiRating — портал с игровой статистикой для игры "Танки Онлайн". 
-      Данный сайт не имеет ничего общего с официальными ресурсами игры и является лишь фан-проектом.
-      <br />
-      В данный момент на портале открыта свободная регистрация.
-    </p>    
-    <p className="fs-5">
-      Главное предназначение этого портала — возможность просмотра игровых данных "в динамике". 
-      В отличии от <a href="https://ratings.tankinline.com">официального сайта рейтингов</a>, на портале можно 
-      посмотреть игровую активность и эффективность пользователя за различные промежутки времени, а также 
-      наблюдать за их изменениями на различных графиках.
-      <br/>
-    </p>
-    <p className="fs-5">
-      Просто зайди на <Link to="/">главную страницу</Link>, и изучай функциональность самостоятельно!
-    </p>
+    <>
+      <p className="fs-5">
+        TankiRating — портал с игровой статистикой для игры "Танки Онлайн".
+        Данный сайт не имеет ничего общего с официальными ресурсами игры и является лишь фан-проектом.
+        <br />
+        В данный момент на портале открыта свободная регистрация.
+      </p>
+      <p className="fs-5">
+        Главное предназначение этого портала — возможность просмотра игровых данных "в динамике".
+        В отличии от <a href="https://ratings.tankinline.com">официального сайта рейтингов</a>, на портале можно
+        посмотреть игровую активность и эффективность пользователя за различные промежутки времени, а также
+        наблюдать за их изменениями на различных графиках.
+        <br />
+      </p>
+      <p className="fs-5">
+        Просто зайди на <Link to="/">главную страницу</Link>, и изучай функциональность самостоятельно!
+      </p>
 
-    <p className="h3 mt-4">
-      Галерея
-    </p>
-  
-    <AboutPictures />
+      <p className="h3 mt-4">
+        Галерея
+      </p>
 
-    <p className="h3 mt-4">
-      Структура портала
-    </p>
+      <AboutPictures />
 
-    <ul className="ms-4">
-      <li><span className="fs-5"><Link to="/">Главная страница</Link> — рейтинг зарегистрированных пользователей</span></li>
-      <li><span className="fs-5"><Link to="/user/Fizzika">Профиль пользователя</Link> — информация о игроке</span></li>
-      <li><span className="fs-5"><Link to="/trends">Тренды</Link> — статистика по всем игрокам-легендам</span></li>
-      <li><span className="fs-5"><Link to="/online">OnlineHub</Link> — график онлаена!!111</span></li>
-    </ul>
+      <p className="h3 mt-4">
+        Структура портала
+      </p>
 
-    <p className="h3 mt-4">
-      Контакты
-    </p>
-    <p className="fs-5">
-      Если ты сменил никнейм, хочешь удалить/заморозить свой аккаунт на портале, ну или просто есть что сказать:  
-    </p>
-    <ul className="ms-4">
-      <li><span className="fs-5">Написать личное сообщение пользователю <b>Fizzika</b> на <a href="https://ru.tankiforum.com/messenger/compose/?to=808" className="href">официальном форуме игры</a> (предпочтительный способ)</span></li>
-      <li><span className="fs-5">Электронная почта <a href="mailto:kdby@riseup.net" className="href">kdby@riseup.net</a></span></li>
-    </ul>
+      <ul className="ms-4">
+        <li><span className="fs-5"><Link to="/">Главная страница</Link> — рейтинг зарегистрированных пользователей</span></li>
+        <li><span className="fs-5"><Link to="/user/Fizzika">Профиль пользователя</Link> — информация о игроке</span></li>
+        <li><span className="fs-5"><Link to="/trends">Тренды</Link> — статистика по всем игрокам-легендам</span></li>
+        <li><span className="fs-5"><Link to="/online">OnlineHub</Link> — график онлаена!!111</span></li>
+      </ul>
 
-   </>
+      <p className="h3 mt-4">
+        Контакты
+      </p>
+      <p className="fs-5">
+        Если ты сменил никнейм, хочешь удалить/заморозить свой аккаунт на портале, ну или просто есть что сказать:
+      </p>
+      <ul className="ms-4">
+        <li><span className="fs-5">Написать личное сообщение пользователю <b>Fizzika</b> на <a href="https://ru.tankiforum.com/messenger/compose/?to=808" className="href">официальном форуме игры</a> (предпочтительный способ)</span></li>
+        <li><span className="fs-5">Электронная почта <a href="mailto:kdby@riseup.net" className="href">kdby@riseup.net</a></span></li>
+      </ul>
+
+      <p className="h3 mt-4">
+        <a name="source">Исходный код, документация</a>
+      </p>
+
+      <p className="fs-5">TankiRating это бесплатный и открытый проект с открытым исходным кодом и общедоступным API</p>
+
+      <ul className="ms-4">
+        <li><span className="fs-5"> <a target="_blank" rel="noopener noreferrer" href="https://github.com/kraskden/tankirating-app" className="href">Client source</a></span></li>
+        <li><span className="fs-5"> <a target="_blank" rel="noopener noreferrer" href="https://github.com/kraskden/tankirating-api" className="href">API source</a></span></li>
+        <li><span className="fs-5"> <a target="_blank" rel="noopener noreferrer" href="https://tankirating.org/api/swagger-ui/index.html" className="href">API documentation</a></span></li>
+      </ul>
+
+    </>
   )
 }
