954788587fe1f35d3436e25706bf607242a1f657
Fizzika <Fizzika.NET@yandex.ru>
2022-12-09 13:31:49 +0300

    Add api url env

---
+++ build-release.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+export REACT_APP_API='/api' 
+
+yarn build
--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.baseURL = process.env.REACT_APP_API ?? 'http://localhost:8080'
 
 axios.interceptors.response.use((response) => response, (err) => {
   if (err.response && err.response.data) {
